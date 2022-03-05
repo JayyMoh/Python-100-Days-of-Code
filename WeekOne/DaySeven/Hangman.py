1a335936e64f81473d0bf4f693c34eeea43d956a
Jonathan Mohr <jonathanmohr@Jonathans-Air.attlocal.net>
2022-03-05 16:56:16 -0500

    hangman starter code

---
+++ WeekOne/DaySeven/Hangman.py
@@ -0,0 +1,18 @@
+from ctypes import pointer
+import random
+# Welcome to Hangman
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = ''
+def getRandomWord():
+    position = random.randrange(0, len(word_list))
+    chosen_word = word_list[position]
+    return chosen_word
+
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
