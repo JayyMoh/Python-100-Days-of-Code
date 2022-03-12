d8a3a4f926a77d6b7c7d51d90712fe345aee0da2
Jonathan <jonmohr@outlook.com>
2022-03-12 18:50:04 -0500

    Silent auction complete

--- WeekTwo/DayTwo/SilentAuction.py
+++ WeekTwo/DayTwo/SilentAuction.py
@@ -1,5 +1,11 @@
 # ===== Silent Auction =====
-print("Welcome to the silent auction program.")
+
+# ===== Imports =====
+import os
+from Art import logo
+
+# ===== Clear console function =====
+clear = lambda: os.system('cls')
 
 # ===== Continue auction condition =====
 continue_auction = True
@@ -7,25 +13,36 @@ continue_auction = True
 # ===== Bids list =====
 bids = {}
 
+# ===== Auction loop =====
 while continue_auction is True:
+    # ===== Welcome =====
+    print("===============================================================")
+    print(logo)
+    print("===============================================================\n")
+    print("Welcome to the silent auction.\n")
+    print("===============================================================")
+
     # ===== Get name =====
-    user_name = raw_input("What is your name?: ")
-    print(user_name)
+    user_name = input("What is your name?: ")
+    # print(user_name)
+
     # ===== Get bid =====
-    user_bid = int(raw_input("What is your bid?: "))
-    print(user_bid)
+    user_bid = int(input("What is your bid?: "))
+    # print(user_bid)
+
     # ===== Add bid to bids list =====
     bids[user_name] = user_bid
-    print(bids)
+    # print(bids)
+
     # ===== Check for more bidders =====
-    more_bidders = raw_input("Are there any more bidders? Type 'yes' or 'no': ").lower()
+    more_bidders = input("Are there any more bidders? Type 'yes' or 'no': ").lower()
     if more_bidders == "no":
         continue_auction = False
-        # Find the winning bid and print it out
+        # ===== Find the winning bid and print it out =====
         winner = max(bids, key=bids.get)
         winning_bid = bids[winner]
-        print("The winner is " + winner + " with a bid of $" + winning_bid + ".")
-        
+        clear()
+        print(f"The winner is {winner} with a bid of ${winning_bid}.")
     elif more_bidders == "yes":
-        # clear the console    
-        print("You need to clear the console")
+        # ===== Clear the console =====
+        clear()
