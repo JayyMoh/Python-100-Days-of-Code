12f9e97923f80192f9ce6c8664c12e3ce791b8dd
Jonathan <jonmohr@outlook.com>
2022-03-14 18:29:23 -0400

    Number guessing game complete

---
+++ WeekTwo/DayFive/NumberGuessingGame.py
@@ -0,0 +1,69 @@
+# ===== Imports =====
+import random
+import os
+# ===== Clear console =====
+clear = lambda: os.system('cls')
+# ===== Get random number between 1 - 100 =====
+numbers = []
+for number in range(1, 101):
+    numbers.append(number)
+def getNumber():
+    random_number = random.choice(numbers)
+    return random_number
+# ===== Get game difficulty =====
+def difficulty():
+    difficulty_level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty_level == "easy":
+        guesses = 10
+    elif difficulty_level == "hard":
+        guesses = 5
+    return guesses
+# ===== Get users guess =====
+def playerGuess():
+    user_guess = int(input("Make a guess: "))
+    print("==========================================================")
+    return user_guess
+# ===== Check users guess against number =====
+def checkGuess(number, number_of_guesses):
+    print("==========================================================")
+    print(f"You have {number_of_guesses} attempts remaining to guess the number.")
+    guess = playerGuess()
+    num_guessed = False
+    while number_of_guesses > 0 and not num_guessed:
+        if guess > number:
+            print(f"{guess} is too high.")
+        elif guess < number:
+            print(f"{guess} is too low.")
+        elif guess == number:
+            num_guessed = True
+            print('You guessed the number!')
+            print("-----------------------")
+            playAgain()
+        number_of_guesses -= 1
+        if number_of_guesses == 0:
+            print("You ran out of attempts.")
+            playAgain()
+        else:
+            checkGuess(number, number_of_guesses)
+# ===== Play again =====
+def playAgain():
+    choice = input("Type 'yes' if you want to play again or type 'no' to exit: ")
+    if choice == 'yes':
+        clear()
+        game()
+    else:
+        return
+# ===== GAME =====
+def game():
+    # ===== Get random number =====
+    random_number = getNumber()
+    # ===== Welcome =====
+    print("==========================================================")
+    print("Welcome to the number guessing game!\nI'm thinking of a number between 1 and 100")
+    # ===== Get difficulty level =====
+    number_of_guesses = difficulty()
+    # ===== Check guess =====
+    checkGuess(random_number, number_of_guesses)
+
+# ===== Calling the game =====
+game()
